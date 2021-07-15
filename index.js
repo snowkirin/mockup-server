dc9964510fa70281756ce59907eafba74c793073
Jinsoo <js@adwitt.com>
2021-07-16 00:44:35 +0900

    - port 변경

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
+app.set('port', (process.env.PORT || 5000))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
@@ -20,6 +20,6 @@ app.get("/api/users", (req, res) => {
 })
 
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(app.get('port'), () => {
+    console.log(`Example app listening at http://localhost:${app.get('port')}`)
 })
