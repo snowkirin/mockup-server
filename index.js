55789c51513a3c64201c8e645b65eb4b72278f01
wisebirds <js@adwitt.com>
2021-07-29 17:58:18 +0900

    koa 적용

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-const express = require('express')
-const app = express()
+// express => koa 로 변경
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+const Koa = require('koa');
+const app = new Koa();
 
-const users = [
-    { id: 1, name: "유저1" },
-    { id: 2, name: "유저2" },
-    { id: 3, name: "유저3" }
-];
+app.use(async ctx => {
+    ctx.body = '안녕. 세상';
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get("/api/users", (req, res) => {
-    res.json({users: users});
-})
+app.listen(3000);
 
 
-app.listen(app.get('port'), () => {
-    console.log(`Example app listening at http://localhost:${app.get('port')}`)
-})
+
+// const express = require('express')
+// const app = express()
+//
+// app.set('port', (process.env.PORT || 5000))
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true}));
+//
+// const users = [
+//     { id: 1, name: "유저1" },
+//     { id: 2, name: "유저2" },
+//     { id: 3, name: "유저3" }
+// ];
+//
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// });
+//
+// app.get("/api/users", (req, res) => {
+//     res.json({users: users});
+// });
+//
+//
+// app.listen(app.get('port'), () => {
+//     console.log(`Example app listening at http://localhost:${app.get('port')}`)
+// });
