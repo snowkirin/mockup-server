d3634fc939efe16a1b29e9ef9e15efc2277d58c3
Jinsoo <js@adwitt.com>
2021-07-30 19:36:35 +0900

    - data 변경

--- api/table/table.controller.js
+++ api/table/table.controller.js
@@ -2,183 +2,153 @@ exports.list = (ctx) => {
     ctx.body = [
         {
             "id": 1,
-            "media": "facebook",
-            "adAccount": "Terry",
-            "business": {
-                "mainCategory": "Cazadero",
-                "subCategory": "Nevada"
-            },
-            "commission": 28,
-            "lastWorkDate": "Mon Feb 24 2020 16:54:07 GMT+0900 (한국 표준시)",
-            "worker": "deirdreterry@lunchpod.com"
+            "media": "google",
+            "adAccount": "Ingram",
+            "mainCategory": "Ironton",
+            "subCategory": "Rhode Island",
+            "commission": 73,
+            "lastWorkDate": "Sat May 16 2020 20:43:59 GMT+0900 (한국 표준시)",
+            "worker": "deirdreingram@lunchpod.com"
         },
         {
             "id": 2,
-            "media": "kakao",
-            "adAccount": "Crosby",
-            "business": {
-                "mainCategory": null,
-                "subCategory": null
-            },
-            "commission": 93,
-            "lastWorkDate": "Thu Sep 17 2020 20:02:55 GMT+0900 (한국 표준시)",
+            "media": "google",
+            "adAccount": "Collins",
+            "mainCategory": null,
+            "subCategory": null,
+            "commission": 15,
+            "lastWorkDate": "Tue Aug 25 2020 22:51:04 GMT+0900 (한국 표준시)",
             "worker": null
         },
         {
             "id": 3,
-            "media": "kakao",
-            "adAccount": "Livingston",
-            "business": {
-                "mainCategory": null,
-                "subCategory": null
-            },
-            "commission": 100,
-            "lastWorkDate": "Sat Feb 22 2020 02:13:47 GMT+0900 (한국 표준시)",
+            "media": "facebook",
+            "adAccount": "Hamilton",
+            "mainCategory": null,
+            "subCategory": null,
+            "commission": 91,
+            "lastWorkDate": "Sat Apr 10 2021 22:45:43 GMT+0900 (한국 표준시)",
             "worker": null
         },
         {
             "id": 4,
-            "media": "kakao",
-            "adAccount": "Knight",
-            "business": {
-                "mainCategory": "Elwood",
-                "subCategory": "New Mexico"
-            },
-            "commission": 42,
-            "lastWorkDate": "Sun Aug 16 2020 03:27:45 GMT+0900 (한국 표준시)",
-            "worker": "deirdreknight@lunchpod.com"
+            "media": "google",
+            "adAccount": "Trujillo",
+            "mainCategory": "Brandermill",
+            "subCategory": "Utah",
+            "commission": 1,
+            "lastWorkDate": "Fri Apr 17 2020 15:48:04 GMT+0900 (한국 표준시)",
+            "worker": "deirdretrujillo@lunchpod.com"
         },
         {
             "id": 5,
-            "media": "kakao",
-            "adAccount": "Aguirre",
-            "business": {
-                "mainCategory": "Brenton",
-                "subCategory": "Virgin Islands"
-            },
-            "commission": 57,
-            "lastWorkDate": "Mon Mar 09 2020 14:44:09 GMT+0900 (한국 표준시)",
-            "worker": "deirdreaguirre@lunchpod.com"
+            "media": "facebook",
+            "adAccount": "Vincent",
+            "mainCategory": "Catharine",
+            "subCategory": "Northern Mariana Islands",
+            "commission": 20,
+            "lastWorkDate": "Mon Mar 16 2020 14:06:27 GMT+0900 (한국 표준시)",
+            "worker": "deirdrevincent@lunchpod.com"
         },
         {
             "id": 6,
-            "media": "google",
-            "adAccount": "Guzman",
-            "business": {
-                "mainCategory": null,
-                "subCategory": null
-            },
-            "commission": 25,
-            "lastWorkDate": "Sat May 23 2020 13:37:41 GMT+0900 (한국 표준시)",
-            "worker": null
+            "media": "kakao",
+            "adAccount": "Horn",
+            "mainCategory": "Emerald",
+            "subCategory": "Kansas",
+            "commission": 42,
+            "lastWorkDate": "Sun Dec 27 2020 15:43:38 GMT+0900 (한국 표준시)",
+            "worker": "deirdrehorn@lunchpod.com"
         },
         {
             "id": 7,
-            "media": "facebook",
-            "adAccount": "French",
-            "business": {
-                "mainCategory": null,
-                "subCategory": null
-            },
-            "commission": 29,
-            "lastWorkDate": "Thu Mar 04 2021 00:49:38 GMT+0900 (한국 표준시)",
+            "media": "kakao",
+            "adAccount": "Pugh",
+            "mainCategory": null,
+            "subCategory": null,
+            "commission": 39,
+            "lastWorkDate": "Wed May 12 2021 13:08:07 GMT+0900 (한국 표준시)",
             "worker": null
         },
         {
             "id": 8,
-            "media": "google",
-            "adAccount": "Smith",
-            "business": {
-                "mainCategory": "Weogufka",
-                "subCategory": "Montana"
-            },
-            "commission": 32,
-            "lastWorkDate": "Tue Feb 04 2020 18:36:01 GMT+0900 (한국 표준시)",
-            "worker": "deirdresmith@lunchpod.com"
+            "media": "kakao",
+            "adAccount": "Mcleod",
+            "mainCategory": null,
+            "subCategory": null,
+            "commission": 45,
+            "lastWorkDate": "Sun Oct 25 2020 02:27:10 GMT+0900 (한국 표준시)",
+            "worker": null
         },
         {
             "id": 9,
-            "media": "kakao",
-            "adAccount": "House",
-            "business": {
-                "mainCategory": null,
-                "subCategory": null
-            },
-            "commission": 25,
-            "lastWorkDate": "Tue Apr 27 2021 00:25:30 GMT+0900 (한국 표준시)",
-            "worker": null
+            "media": "google",
+            "adAccount": "Lowe",
+            "mainCategory": "Yettem",
+            "subCategory": "Texas",
+            "commission": 92,
+            "lastWorkDate": "Fri Jun 18 2021 22:41:26 GMT+0900 (한국 표준시)",
+            "worker": "deirdrelowe@lunchpod.com"
         },
         {
             "id": 10,
-            "media": "google",
-            "adAccount": "Figueroa",
-            "business": {
-                "mainCategory": null,
-                "subCategory": null
-            },
-            "commission": 48,
-            "lastWorkDate": "Thu May 20 2021 01:01:54 GMT+0900 (한국 표준시)",
-            "worker": null
+            "media": "kakao",
+            "adAccount": "Baldwin",
+            "mainCategory": "Odessa",
+            "subCategory": "Alabama",
+            "commission": 25,
+            "lastWorkDate": "Sat Apr 11 2020 08:52:44 GMT+0900 (한국 표준시)",
+            "worker": "deirdrebaldwin@lunchpod.com"
         },
         {
             "id": 11,
             "media": "facebook",
-            "adAccount": "Brown",
-            "business": {
-                "mainCategory": null,
-                "subCategory": null
-            },
-            "commission": 6,
-            "lastWorkDate": "Fri May 08 2020 11:05:53 GMT+0900 (한국 표준시)",
-            "worker": null
+            "adAccount": "Murphy",
+            "mainCategory": "Nettie",
+            "subCategory": "California",
+            "commission": 36,
+            "lastWorkDate": "Mon Jun 08 2020 23:56:10 GMT+0900 (한국 표준시)",
+            "worker": "deirdremurphy@lunchpod.com"
         },
         {
             "id": 12,
             "media": "facebook",
-            "adAccount": "Reyes",
-            "business": {
-                "mainCategory": "Stollings",
-                "subCategory": "Palau"
-            },
-            "commission": 15,
-            "lastWorkDate": "Wed Nov 18 2020 18:19:08 GMT+0900 (한국 표준시)",
-            "worker": "deirdrereyes@lunchpod.com"
+            "adAccount": "Poole",
+            "mainCategory": "Downsville",
+            "subCategory": "Idaho",
+            "commission": 45,
+            "lastWorkDate": "Tue Feb 25 2020 05:52:08 GMT+0900 (한국 표준시)",
+            "worker": "deirdrepoole@lunchpod.com"
         },
         {
             "id": 13,
-            "media": "kakao",
-            "adAccount": "Franco",
-            "business": {
-                "mainCategory": "Venice",
-                "subCategory": "Wisconsin"
-            },
-            "commission": 90,
-            "lastWorkDate": "Fri Apr 03 2020 11:23:57 GMT+0900 (한국 표준시)",
-            "worker": "deirdrefranco@lunchpod.com"
+            "media": "facebook",
+            "adAccount": "Delgado",
+            "mainCategory": null,
+            "subCategory": null,
+            "commission": 83,
+            "lastWorkDate": "Thu May 07 2020 06:53:05 GMT+0900 (한국 표준시)",
+            "worker": null
         },
         {
             "id": 14,
-            "media": "google",
-            "adAccount": "Mckinney",
-            "business": {
-                "mainCategory": null,
-                "subCategory": null
-            },
+            "media": "facebook",
+            "adAccount": "Winters",
+            "mainCategory": null,
+            "subCategory": null,
             "commission": 86,
-            "lastWorkDate": "Thu Jun 25 2020 07:49:57 GMT+0900 (한국 표준시)",
+            "lastWorkDate": "Wed Apr 21 2021 22:46:10 GMT+0900 (한국 표준시)",
             "worker": null
         },
         {
             "id": 15,
             "media": "kakao",
-            "adAccount": "Eaton",
-            "business": {
-                "mainCategory": null,
-                "subCategory": null
-            },
-            "commission": 53,
-            "lastWorkDate": "Sun May 02 2021 09:47:15 GMT+0900 (한국 표준시)",
-            "worker": null
+            "adAccount": "Pacheco",
+            "mainCategory": "Malo",
+            "subCategory": "Virgin Islands",
+            "commission": 81,
+            "lastWorkDate": "Tue Feb 25 2020 19:40:56 GMT+0900 (한국 표준시)",
+            "worker": "deirdrepacheco@lunchpod.com"
         }
     ]
 };
